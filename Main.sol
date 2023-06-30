38661f6cd8a80364ff06ccb473aba3d04bb2cdab
haub3902 <131357339+haub3902@users.noreply.github.com>
2023-07-01 01:49:12 +0300

    Update Main.sol

--- Main.sol
+++ Main.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.8.0;
+
+// Struct to hold identity information
+struct Identity {
+    string name;
+    uint age;
+}
+
+// Mapping of addresses to their corresponding identities
+mapping(address => Identity) public identities;
+
+// Function to verify the identity of a user
+function verifyIdentity(string memory name, uint age) public {
+    require(age >= 18, "You must be 18 or older to verify your identity");
+    identities[msg.sender] = Identity(name, age);
+}
 
